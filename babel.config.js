94459eb56279bfe850e3bafeb389c680e5999294
rdzsebastian <rdzsebastian@gmail.com>
2025-09-25 12:22:36 -0300

    Ajustes finales de configuracion para deploy en play store android

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [
-      "react-native-reanimated/plugin", // debe ir al final
-    ],
+  plugins: ['react-native-worklets/plugin'],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,23 @@ const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-  // [CRUCIAL] This configures the eager bundler for faster local builds
-  isEager: true,
+  isEager: true, // acelera builds locales
 });
 
-// Configuración para SVG
+// Transformador para SVG
 config.transformer = {
   ...config.transformer,
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
 };
+
+// Resolver SVG
 config.resolver = {
   ...config.resolver,
   assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
   sourceExts: [...config.resolver.sourceExts, 'svg'],
 };
+
+// No incluir opciones obsoletas
+// watcher.unstable_lazySha1, watcher.unstable_workerThreads, watcher.unstable_autoSaveCache, server.forwardClientLogs
 
 module.exports = withNativeWind(config, { input: './app/globals.css' });
